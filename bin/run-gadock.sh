bfc94c047e2b491f085e70b4513aebd62c1b4578
rvhonorato <rvhonorato@gmail.com>
2020-05-24 13:15:18 +0200

    Update run-gadock.sh

--- bin/run-gadock.sh
+++ bin/run-gadock.sh
@@ -1,9 +1,9 @@
 #PBS -N GADock
 #PBS -q medium
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=48
 #PBS -S /bin/tcsh
 
 cd /home/rodrigo/repos/gadock
 setenv PYTHONPATH ${PYTHONPATH}:`pwd`
 conda activate gadock
-python bin/gadock.py target-unbound.pdb 10 25 1 > gadock.out
+python bin/gadock.py target-unbound.pdb 48 > gadock.out
