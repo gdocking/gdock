fccbabdaf1bc3a1a275a162725a5c410b0b23d5e
rvhonorato <rvhonorato@gmail.com>
2020-10-02 10:13:32 +0200

    implement logging

--- bin/gadock.py
+++ bin/gadock.py
@@ -9,7 +9,7 @@ import logging
 ga_log = logging.getLogger('ga_log')
 ga_log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-formatter = logging.Formatter( '%(asctime)s %(levelname)s - %(message)s')
+formatter = logging.Formatter( '%(asctime)s %(module)s:%(lineno)d %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 ga_log.addHandler(ch)
 
@@ -20,26 +20,29 @@ if __name__ == '__main__':
     parser.add_argument("input_file")
     args = parser.parse_args()
 
+    ga_log.info('Setting up simulation')
     s = Setup(args.input_file)
     if not s.validate():
         # raise GADockError
-        logging.error('Validation failed!')
+        ga_log.error('Validation failed!')
         exit()
 
-    ga_log.info('Initializing')
     run_params = s.initialize()
 
     # 1. Load structure
+    ga_log.info('Loading structures')
     input_molecules = PDB()
     input_molecules.load(run_params['mol_a'])
     input_molecules.load(run_params['mol_b'])
 
     # 2. Load restraints
+    ga_log.info('Loading restraints')
     restraints = Restraint(input_molecules.raw_pdb)
     restraints.load(run_params['restraints_a'], 'A')
     restraints.load(run_params['restraints_b'], 'B')
 
     # 3. Position
+    ga_log.info('Loading geometry')
     geo = Geometry(input_molecules, restraints)
     geo.initial_position()
 
