27009624a88a99a5b65b978a22d32622e8f2cf57
rvhonorato <rvhonorato@gmail.com>
2020-11-13 16:16:46 +0100

    Revert "code linting"
    This reverts commit 91b481d3c85a295ede328c01d6fb78ed758dbe51.

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,4 +1,3 @@
-"""Analysis module."""
 import configparser
 import os
 import subprocess  # nosec
@@ -17,7 +16,6 @@ fcc = ini.get('third_party', 'fcc_path')
 
 
 class Analysis:
-    """This is the main Analysis class."""
 
     def __init__(self, input_structures, result_dic, run_params):
         """Setup the analysis class."""
@@ -34,12 +32,12 @@ class Analysis:
         for line in self.input_structures.split('\n'):
             if line.startswith('ATOM'):
                 chain = line[21]
-                x_coord = float(line[31:38])
-                y_coord = float(line[39:46])
-                z_coord = float(line[47:54])
+                x = float(line[31:38])
+                y = float(line[39:46])
+                z = float(line[47:54])
                 if chain not in input_structure_dic:
                     input_structure_dic[chain] = {'coord': [], 'raw': []}
-                input_structure_dic[chain]['coord'].append((x_coord, y_coord, z_coord))
+                input_structure_dic[chain]['coord'].append((x, y, z))
                 input_structure_dic[chain]['raw'].append(line)
 
         # use the chromossome and create the structure!
@@ -50,25 +48,22 @@ class Analysis:
                 # transform
                 rot = R.from_euler('zyx', individual[:3])
                 transl = individual[3:]
-                ligand_coords = np.array(input_structure_dic['B']['coord'])
-                center = ligand_coords.mean(axis=0)
-                ligand_coords -= center
-                ligand_coords -= transl
-                rotated_coords = np.array([rot.apply(e) for e in ligand_coords])
-                rotated_coords += center
-                input_structure_dic['B']['coord'] = list(rotated_coords)
+                c = np.array(input_structure_dic['B']['coord'])
+                center = c.mean(axis=0)
+                c -= center
+                c -= transl
+                r = np.array([rot.apply(e) for e in c])
+                r += center
+                input_structure_dic['B']['coord'] = list(r)
 
-                gen_str = str(gen).rjust(3, '0')
-                idx_str = str(idx).rjust(3, '0')
-                pdb_name = f"{self.analysis_path}/{gen_str}_{idx_str}.pdb"
-                with open(pdb_name, 'w') as file_handle:
+                pdb_name = f"{self.analysis_path}/{str(gen).rjust(3, '0')}_{str(idx).rjust(3, '0')}.pdb"
+                with open(pdb_name, 'w') as fh:
                     for chain in input_structure_dic:
-                        for coord, line in zip(input_structure_dic[chain]['coord'], \
-                                input_structure_dic[chain]['raw']):
+                        for coord, line in zip(input_structure_dic[chain]['coord'], input_structure_dic[chain]['raw']):
                             new_x, new_y, new_z = format_coords(coord)
                             new_line = f'{line[:30]} {new_x} {new_y} {new_z} {line[55:]}\n'
-                            file_handle.write(new_line)
-                file_handle.close()
+                            fh.write(new_line)
+                fh.close()
                 self.structure_list.append(pdb_name)
 
         self.structure_list.sort()
@@ -80,15 +75,14 @@ class Analysis:
         # TODO: Make this run using multiple processors
         make_contacts = f'{fcc}/scripts/make_contacts.py'
         pdb_list = f'{self.analysis_path}/pdb.list'
-        with open(pdb_list, 'w') as file_handle:
+        with open(pdb_list, 'w') as fh:
             for pdb in self.structure_list:
-                file_handle.write(f'{pdb}\n')
-        file_handle.close()
+                fh.write(f'{pdb}\n')
+        fh.close()
 
         cmd = f'{make_contacts} -f {pdb_list}'
-        ga_log.debug('cmd is: %s', cmd)
-        out = subprocess.check_output(shlex.split(cmd), shell=False,
-                                        stderr=subprocess.PIPE)  # nosec
+        ga_log.debug(f'cmd is: {cmd}')
+        out = subprocess.check_output(shlex.split(cmd), shell=False, stderr=subprocess.PIPE)  # nosec
         if 'Finished' not in out.decode('utf-8'):
             ga_log.error('FCC - make_contacts.py failed')
             exit()
@@ -97,11 +91,11 @@ class Analysis:
         calc_fcc_matrix = f'{fcc}/scripts/calc_fcc_matrix.py'
         contact_list = f'{self.analysis_path}/contact.list'
         fcc_matrix = f'{self.analysis_path}/fcc.matrix'
-        with open(contact_list, 'w') as file_handle:
+        with open(contact_list, 'w') as fh:
             for pdb in self.structure_list:
                 contact_fname = pdb.replace('.pdb', '.contacts')
-                file_handle.write(f'{contact_fname}\n')
-        file_handle.close()
+                fh.write(f'{contact_fname}\n')
+        fh.close()
 
         cmd = f'{calc_fcc_matrix} -f {contact_list} -o {fcc_matrix}'
         subprocess.check_output(shlex.split(cmd), shell=False, stderr=subprocess.PIPE)  # nosec
@@ -109,7 +103,7 @@ class Analysis:
             ga_log.error('FCC - calc_fcc_matrix.py failed')
             exit()
 
-        ga_log.info('FCC - Clustering with cutoff=%.2f', cutoff)
+        ga_log.info(f'FCC - Clustering with cutoff={cutoff}')
         cluster_fcc = f'{fcc}/scripts/cluster_fcc.py'
         cluster_out = f'{self.analysis_path}/cluster.out'
         cmd = f'{cluster_fcc} {fcc_matrix} {cutoff} -o {cluster_out}'
@@ -118,28 +112,28 @@ class Analysis:
             ga_log.warning('No clusters were written!')
             return
         else:
-            with open(cluster_out, 'r') as file_handle:
-                for line in file_handle.readlines():
+            with open(cluster_out, 'r') as fh:
+                for line in fh.readlines():
                     data = line.split()
                     cluster_id = int(data[1])
                     self.cluster_dic[cluster_id] = []
                     cluster_elements = map(int, data[4:])
                     for element in cluster_elements:
-                        e_name = os.path.split(self.structure_list[element])[1].split('.pdb')[0]
-                        self.cluster_dic[cluster_id].append(e_name)
-            ga_log.info('FCC - %i clusters identified', len(self.cluster_dic))
+                        element_name = os.path.split(self.structure_list[element])[1].split('.pdb')[0]
+                        self.cluster_dic[cluster_id].append(element_name)
+            ga_log.info(f'FCC - {len(self.cluster_dic)} clusters identified')
+
+        return self.cluster_dic
 
     def output(self):
         """Generate a script friendly output table."""
         output_f = f'{self.analysis_path}/gdock.dat'
-        ga_log.info('Saving output file to %s', output_f)
-        with open(output_f, 'w') as file_handle:
-            file_handle.write('generation,individual,fitness\n')
+        ga_log.info(f'Saving output file to {output_f}')
+        with open(output_f, 'w') as fh:
+            fh.write('generation,individual,fitness\n')
             for generation in self.result_dic:
                 for individual in self.result_dic[generation]:
                     _, fitness = self.result_dic[generation][individual]
                     fitness = fitness[0]  # placeholder for multiple values of fitnessess
-                    generation_str = str(generation).rjust(3, '0')
-                    individual_str = str(individual).rjust(3, '0')
-                    file_handle.write(f"{generation_str},{individual_str},{fitness}\n")
-        file_handle.close()
+                    fh.write(f"{str(generation).rjust(3, '0')},{str(individual).rjust(3, '0')},{fitness}\n")
+        fh.close()
