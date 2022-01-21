eff4c645b0d667fb00cf7a7fa5b98dfd4ec2430a
rodrigo <rvhonorato@gmail.com>
2022-01-21 15:30:55 +0100

    code style

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,42 +1,43 @@
 import configparser
-import os
-import math
-import pathlib
-import multiprocessing
-import sys
-from utils.files import get_full_path
-from modules.profit import Profit
 import logging
-ga_log = logging.getLogger('ga_log')
+import math
+import multiprocessing
+import os
+import pathlib
+import sys
 
-etc_folder = get_full_path('etc')
+from modules.profit import Profit
+from utils.files import get_full_path
+
+ga_log = logging.getLogger("ga_log")
+
+etc_folder = get_full_path("etc")
 ini = configparser.ConfigParser(os.environ)
-ini.read(os.path.join(etc_folder, 'gdock.ini'), encoding='utf-8')
-fcc_path = ini.get('third_party', 'fcc_path')
+ini.read(os.path.join(etc_folder, "gdock.ini"), encoding="utf-8")
+fcc_path = ini.get("third_party", "fcc_path")
 
 sys.path.append(f"{fcc_path}/scripts")
 
 try:
     import calc_fcc_matrix
-    import make_contacts
     import cluster_fcc
+    import make_contacts
 except Exception as e:
-    ga_log.error('FCC could not be imported')
+    ga_log.error("FCC could not be imported")
     ga_log.error(e)
     sys.exit()
 
 
 class Analysis:
-
     def __init__(self, result_dic, run_params):
         """Setup the analysis class."""
         self.result_dic = result_dic
         self.analysis_path = f"{run_params['folder']}/analysis"
-        if 'native' in run_params:
-            self.native = run_params['native']
+        if "native" in run_params:
+            self.native = run_params["native"]
         else:
-            self.native = ''
-        self.nproc = run_params['np']
+            self.native = ""
+        self.nproc = run_params["np"]
         self.structure_list = self.get_structures(result_dic)
         self.cluster_dic = {}
         self.irmsd_dic = {}
@@ -48,15 +49,15 @@ class Analysis:
         structure_score_l = []
         for gen in data_dic:
             for ind in data_dic[gen]:
-                struct = data_dic[gen][ind]['structure']
+                struct = data_dic[gen][ind]["structure"]
                 if struct:
-                    if 'score' not in data_dic[gen][ind]:
-                        raise Exception('Structure does not contain score,'
-                                        ' cannot proceed.')
-                    score = data_dic[gen][ind]['score']
+                    if "score" not in data_dic[gen][ind]:
+                        raise Exception(
+                            "Structure does not contain score," " cannot proceed."
+                        )
+                    score = data_dic[gen][ind]["score"]
                     structure_score_l.append((struct, score))
-        sorted_structure_score_l = sorted(structure_score_l,
-                                          key=lambda x: x[1])
+        sorted_structure_score_l = sorted(structure_score_l, key=lambda x: x[1])
 
         # get only the structures
         structure_l = [e[0] for e in sorted_structure_score_l]
@@ -65,49 +66,46 @@ class Analysis:
     @staticmethod
     def calc_contact(executable, pdb_f, cutoff=5.0):
         # fcc-wrapper
-        contact_f = pdb_f.replace('.pdb', '.contacts')
+        contact_f = pdb_f.replace(".pdb", ".contacts")
         make_contacts._calculate_contacts(executable, pdb_f, str(cutoff))
         if os.path.isfile(contact_f):
             return contact_f
         else:
-            return ''
+            return ""
 
     def cluster(self, cutoff=0.75, top=1000):
         """Use FCC to cluster structures."""
 
         # Calculate contacts
-        ga_log.info('FCC - Calculating contacts')
+        ga_log.info("FCC - Calculating contacts")
         pool = multiprocessing.Pool(processes=self.nproc)
         input_structure_l = self.structure_list[:top]
         for pdb in input_structure_l:
-            pool.apply_async(self.calc_contact, args=(self.contact_executable,
-                                                      pdb))
+            pool.apply_async(self.calc_contact, args=(self.contact_executable, pdb))
 
         pool.close()
         pool.join()
 
         contact_file_l = []
         for pdb in input_structure_l:
-            contact_f = pdb.replace('.pdb', '.contacts')
+            contact_f = pdb.replace(".pdb", ".contacts")
             if os.path.isfile(contact_f):
                 contact_file_l.append(contact_f)
 
         if not contact_file_l:
-            ga_log.warning('No contacts were calculated')
+            ga_log.warning("No contacts were calculated")
 
         # Calculate matrix
-        ga_log.info('FCC - Calculating matrix')
-        parsed_contacts = calc_fcc_matrix.parse_contact_file(contact_file_l,
-                                                             False)
+        ga_log.info("FCC - Calculating matrix")
+        parsed_contacts = calc_fcc_matrix.parse_contact_file(contact_file_l, False)
 
         # matrix is a generator object, be careful with it
-        matrix = calc_fcc_matrix.calculate_pairwise_matrix(parsed_contacts,
-                                                           False)
+        matrix = calc_fcc_matrix.calculate_pairwise_matrix(parsed_contacts, False)
 
         # write it to a file, so we can read it afterwards and don't
         #  need to reinvent the wheel
-        fcc_matrix_f = f'{self.analysis_path}/fcc.matrix'
-        with open(fcc_matrix_f, 'w') as fh:
+        fcc_matrix_f = f"{self.analysis_path}/fcc.matrix"
+        with open(fcc_matrix_f, "w") as fh:
             for data in list(matrix):
                 data_str = f"{data[0]} {data[1]} {data[2]:.2f} {data[3]:.3f}"
                 data_str += os.linesep
@@ -115,20 +113,20 @@ class Analysis:
         fh.close()
 
         # cluster
-        ga_log.info('FCC - Clustering')
+        ga_log.info("FCC - Clustering")
         pool = cluster_fcc.read_matrix(fcc_matrix_f, cutoff, strictness=0.75)
         _, clusters = cluster_fcc.cluster_elements(pool, 4)
 
         if clusters:
-            ga_log.info(f'FCC - {len(clusters)} clusters identified')
+            ga_log.info(f"FCC - {len(clusters)} clusters identified")
             # use fcc's output
-            cluster_out = f'{self.analysis_path}/cluster.out'
-            with open(cluster_out, 'w') as fh:
+            cluster_out = f"{self.analysis_path}/cluster.out"
+            with open(cluster_out, "w") as fh:
                 cluster_fcc.output_clusters(fh, clusters)
             fh.close()
 
             # read it again!
-            with open(cluster_out, 'r') as fh:
+            with open(cluster_out, "r") as fh:
                 for line in fh.readlines():
                     data = line.split()
                     cluster_id = int(data[1])
@@ -139,70 +137,68 @@ class Analysis:
                         element_name = pathlib.Path(structure_name).stem
                         self.cluster_dic[cluster_id].append(element_name)
         else:
-            ga_log.info('FCC - No clusters identified')
+            ga_log.info("FCC - No clusters identified")
 
         return self.cluster_dic
 
     def evaluate(self):
         """Use PROFIT to calculate the interface rmsd."""
         try:
-            ga_log.info('Using PROFIT to evaluate the irmsd')
-            profit = Profit(ref=self.native,
-                            mobi=self.structure_list,
-                            nproc=self.nproc)
+            ga_log.info("Using PROFIT to evaluate the irmsd")
+            profit = Profit(ref=self.native, mobi=self.structure_list, nproc=self.nproc)
             self.irmsd_dic = profit.calc_irmsd()
         except Exception as e:
             ga_log.warning(e)
-            ga_log.warning('Skipping evaluation')
+            ga_log.warning("Skipping evaluation")
 
     def output(self):
         """Generate a script friendly output table."""
-        output_f = f'{self.analysis_path}/gdock.dat'
-        ga_log.info(f'Saving output file to {output_f}')
+        output_f = f"{self.analysis_path}/gdock.dat"
+        ga_log.info(f"Saving output file to {output_f}")
 
-        sep = '\t'
-        header = 'gen' + sep
-        header += 'ind' + sep
-        header += 'ranking' + sep
-        header += 'score' + sep
-        header += 'fitness' + sep
-        header += 'energy' + sep
-        header += 'irmsd' + sep
-        header += 'cluster_id' + sep
-        header += 'internal_cluster_ranking' + sep
-        header += 'structure_path' + os.linesep
+        sep = "\t"
+        header = "gen" + sep
+        header += "ind" + sep
+        header += "ranking" + sep
+        header += "score" + sep
+        header += "fitness" + sep
+        header += "energy" + sep
+        header += "irmsd" + sep
+        header += "cluster_id" + sep
+        header += "internal_cluster_ranking" + sep
+        header += "structure_path" + os.linesep
 
-        with open(output_f, 'w') as fh:
+        with open(output_f, "w") as fh:
             fh.write(header)
             for gen in self.result_dic:
                 for ind in self.result_dic[gen]:
-                    fitness_values = self.result_dic[gen][ind]['fitness']
+                    fitness_values = self.result_dic[gen][ind]["fitness"]
 
                     # placeholder for multiple values of fitnessess
                     fitness = fitness_values[0]
 
-                    score = self.result_dic[gen][ind]['score']
-                    ranking = self.result_dic[gen][ind]['ranking']
-                    energy = self.result_dic[gen][ind]['energy']
+                    score = self.result_dic[gen][ind]["score"]
+                    ranking = self.result_dic[gen][ind]["ranking"]
+                    energy = self.result_dic[gen][ind]["energy"]
 
-                    generation_str = str(gen).rjust(4, '0')
-                    individual_str = str(ind).rjust(4, '0')
+                    generation_str = str(gen).rjust(4, "0")
+                    individual_str = str(ind).rjust(4, "0")
 
-                    structure = self.result_dic[gen][ind]['structure']
+                    structure = self.result_dic[gen][ind]["structure"]
                     if structure:
                         structure_id = pathlib.Path(structure).stem
                         try:
                             irmsd = self.irmsd_dic[structure_id]
                         except KeyError:
-                            irmsd = float('nan')
+                            irmsd = float("nan")
                     else:
-                        irmsd = float('nan')
+                        irmsd = float("nan")
 
                     # Add the clustering information
                     # TODO: Improve this part, its very messy.
-                    model = f'{generation_str}_{individual_str}'
-                    internal_ranking = float('nan')
-                    cluster_id = float('nan')
+                    model = f"{generation_str}_{individual_str}"
+                    internal_ranking = float("nan")
+                    cluster_id = float("nan")
                     if self.cluster_dic:
                         for cluster_id in self.cluster_dic:
                             subcluster = self.cluster_dic[cluster_id]
@@ -211,7 +207,7 @@ class Analysis:
                                 internal_ranking = subcluster.index(model) + 1
                                 break
                     if math.isnan(internal_ranking):
-                        cluster_id = float('nan')
+                        cluster_id = float("nan")
 
                     output_str = f"{gen}" + sep
                     output_str += f"{ind}" + sep
--- tools/run-benchmark.py
+++ tools/run-benchmark.py
@@ -1,72 +1,75 @@
 # Deamon to run the benchmark!
+import argparse
 import glob
+import logging
 import os
+import shlex
+import subprocess  # nosec
 import sys
 from pathlib import Path
-import logging
-import argparse
-import subprocess  # nosec
-import shlex
 
-bm_log = logging.getLogger('bm_log')
+bm_log = logging.getLogger("bm_log")
 bm_log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-formatter = logging.Formatter(' %(asctime)s %(module)s:%(lineno)d'
-                              ' %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    " %(asctime)s %(module)s:%(lineno)d" " %(levelname)s - %(message)s"
+)
 ch.setFormatter(formatter)
 bm_log.addHandler(ch)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("gdockbm_path", help=("Full path of the root directory"
-                                              " of the prepared folders"))
+    parser.add_argument(
+        "gdockbm_path",
+        help=("Full path of the root directory" " of the prepared folders"),
+    )
     args = parser.parse_args()
 
     benchmark_path = Path(args.gdockbm_path).resolve()
     if not benchmark_path.exists():
-        bm_log.error(f'Path {benchmark_path} does not exist.')
+        bm_log.error(f"Path {benchmark_path} does not exist.")
         sys.exit()
 
     # FIXME: There is probably a better way of doing this
     dirname, filename = os.path.split(os.path.abspath(__file__))
-    gdock_exe = f'{dirname}/../gdock.py'
+    gdock_exe = f"{dirname}/../gdock.py"
     python_exe = sys.executable
 
-    benchmark = [f for f in glob.glob(f'{benchmark_path}/*') if '.' not in f]
+    benchmark = [f for f in glob.glob(f"{benchmark_path}/*") if "." not in f]
     benchmark.sort()
 
     total = len(benchmark)
 
     for i, folder in enumerate(benchmark):
         target_name = Path(folder).name
-        output_file = f'{folder}/run/analysis/gdock.dat'
+        output_file = f"{folder}/run/analysis/gdock.dat"
         if not os.path.isfile(output_file):
 
-            bm_log.info(f'{target_name} Running - Remaining {total-i}')
+            bm_log.info(f"{target_name} Running - Remaining {total-i}")
 
             os.chdir(folder)
-            bm_log.debug(f'chdir {folder}')
+            bm_log.debug(f"chdir {folder}")
 
-            cmd = (f'{python_exe} {gdock_exe} run.toml')
-            bm_log.debug(f'cmd is: {cmd}')
-            result = subprocess.run(shlex.split(cmd),
-                                    capture_output=True,
-                                    shell=False)  # nosec
+            cmd = f"{python_exe} {gdock_exe} run.toml"
+            bm_log.debug(f"cmd is: {cmd}")
+            result = subprocess.run(
+                shlex.split(cmd), capture_output=True, shell=False
+            )  # nosec
 
             # Make this explicit so eventually we can capture
             #  stuff from stderr/out
-            stdout = result.stdout.decode('utf-8')
-            stderr = result.stderr.decode('utf-8')
+            stdout = result.stdout.decode("utf-8")
+            stderr = result.stderr.decode("utf-8")
 
-            with open(f'{folder}/gdock.out', 'w') as out_fh:
+            with open(f"{folder}/gdock.out", "w") as out_fh:
                 out_fh.write(stderr)
             out_fh.close()
 
             if not os.path.isfile(output_file):
-                bm_log.error(f'Something wrong with {target_name}')
-                with open(f'{folder}/FAIL', 'w') as fail_fh:
-                    fail_fh.write('I failed :(')
+                bm_log.error(f"Something wrong with {target_name}")
+                with open(f"{folder}/FAIL", "w") as fail_fh:
+                    fail_fh.write("I failed :(")
                 fail_fh.close()
 
         else:
-            bm_log.info(f'{target_name} DONE - Remaining {total-i}')
+            bm_log.info(f"{target_name} DONE - Remaining {total-i}")
