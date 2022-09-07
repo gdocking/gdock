51c9bf555d0c07aa92d979181ec168d7cc03c292
rvhonorato <rvhonorato@users.noreply.github.com>
2022-09-07 07:01:46 +0000

    [SKIP] version bump 1.2.1 -> 1.2.2
    Update README.md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="gdock",
     license="BSD Zero Clause (0BSD)",
-    version="1.2.1",
+    version="1.2.2",
     author="Rodrigo V. Honorato",
     description="Genetic Algorithm applied to Protein-Protein Docking",
     author_email="rvhonorato@protonmail.com",
